0dff7de13d142f2493555a33933515e95bc28010
TheStarBars <aoges@orange.fr>
2024-10-29 16:21:10 +0100

    [US-3] add modify form for address and motivation

--- lib/Vue/profile.dart
+++ lib/Vue/profile.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+const List<String> motivations = [
+  "Poursuite d'études",
+  "Reconversion professionnelle",
+  "Réorientation"
+];
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key, required this.title});
 
@@ -12,6 +18,7 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  String motivationValue = motivations.first;
 
   String lastName = "monNom";
   String firstName = "monPrenom";
@@ -20,6 +27,44 @@ class _ProfilePageState extends State<ProfilePage> {
   String address = "maMaison";
   String motivation = "maMotivation";
 
+  Widget modifyAddress() {
+    return Column(children: [
+      TextFormField(
+        decoration: const InputDecoration(
+          labelText: "Modify Address",),
+        validator: (value) => value!.isEmpty ? 'Enter your last name' : null,
+      ),
+      ElevatedButton(
+          child: const Text('Modify'),
+          onPressed: () => ()
+      )
+    ]
+    );
+  }
+
+
+  Widget modifyMotivation() {
+    return Column(children: [
+      DropdownButton(
+          isExpanded: true,
+          value: motivationValue,
+          items: motivations.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (value) => setState(() {
+            motivationValue = value!;
+          })),
+      ElevatedButton(
+          child: const Text('Modify'),
+          onPressed: () => ()
+      )
+    ]
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,10 +72,10 @@ class _ProfilePageState extends State<ProfilePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Container(
-          width: double.infinity,
-          margin: const EdgeInsets.all(16.0),
-          child: Card(
+      body: Column(
+        // width: double.infinity,
+        // margin: const EdgeInsets.all(16.0),
+          children: [ Card(
               margin: EdgeInsets.all(10.0),
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
@@ -49,7 +94,13 @@ class _ProfilePageState extends State<ProfilePage> {
                     Text(motivation),
                   ],
                 ),
-              ))),
+              )
+          ),
+            Form(child: modifyAddress()),
+            Form(child: modifyMotivation())
+
+          ]
+      ),
     );
   }
 }
