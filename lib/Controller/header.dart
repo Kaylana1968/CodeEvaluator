602e8d064fc8db4daa4b79630432e96459676c10
DevMehd <60779348+DevMehd@users.noreply.github.com>
2024-10-30 16:19:21 +0100

    [US-0] Update headerDisplay to include back button option in AppBar

--- lib/Controller/header.dart
+++ lib/Controller/header.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
- AppBar headerDisplay(BuildContext context, String title) {
-   return AppBar(
-     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-     title: Text(title),
-     actions: [
-       IconButton(
-         style: IconButton.styleFrom(backgroundColor: Colors.deepPurple,
-           foregroundColor: Colors.black,
-         ),
-         onPressed: () => (Navigator.pushNamed(context, '/profile')),
-         icon: const Icon(
-           Icons.person,
-           size: 30,
-         ),
-       ),
-     ],
-   );
- }
+AppBar headerDisplay(BuildContext context, String title, bool BackButton) {
+  return AppBar(
+    backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    title: Text(title),
+    automaticallyImplyLeading: BackButton,
+    actions: [
+      IconButton(
+        style: IconButton.styleFrom(
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.black,
+        ),
+        onPressed: () => (Navigator.pushNamed(context, '/profile')),
+        icon: const Icon(
+          Icons.person,
+          size: 30,
+        ),
+      ),
+    ],
+  );
+}
